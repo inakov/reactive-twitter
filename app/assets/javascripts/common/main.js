9935b8ff45c17e71228850f3cb1f77ad825ad650
inakov <ivannakov@hotmail.com>
2015-01-20 13:16:12 +0200

    - Created profile card directive

--- app/assets/javascripts/common/main.js
+++ app/assets/javascripts/common/main.js
@@ -1,10 +1,10 @@
 /**
  * Common functionality.
  */
-define(['angular', './services/helper', './services/playRoutes', './filters', './directives/example'],
+define(['angular', './services/helper', './services/playRoutes', './filters', './directives/example', './directives/profileCard'],
     function(angular) {
   'use strict';
 
   return angular.module('yourprefix.common', ['common.helper', 'common.playRoutes', 'common.filters',
-    'common.directives.example']);
+    'common.directives.example','common.directives.profileCard']);
 });
