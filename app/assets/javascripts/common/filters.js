89d44df5e8081c7f84fe481452a679f591ffaac5
inakov <ivannakov@hotmail.com>
2015-02-13 14:41:31 +0200

    - hashtags text filter

--- app/assets/javascripts/common/filters.js
+++ app/assets/javascripts/common/filters.js
@@ -3,18 +3,12 @@ define(['angular'], function(angular) {
   'use strict';
 
   var mod = angular.module('common.filters', []);
-  /**
-   * Extracts a given property from the value it is applied to.
-   * {{{
-   * (user | property:'name')
-   * }}}
-   */
-  mod.filter('property', function(value, property) {
-    if (angular.isObject(value)) {
-      if (value.hasOwnProperty(property)) {
-        return value[property];
-      }
-    }
-  });
+
+    mod.filter('hashtags', ['$sce', function($sce){
+        return function(value) {
+            $sce.isTrusted
+            return $sce.trustAsHtml(value.replace(/(#)(\S*)/g,'<a href="#/discover">$1$2</a>'));
+        }
+    }]);
   return mod;
 });
